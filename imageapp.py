84c28a9c472bfb16be10a057f35cb29578025f9b
Karthikeyan018 <121956946+Karthikeyan018@users.noreply.github.com>
2024-06-05 00:51:50 +0530

    Update imageapp.py

--- imageapp.py
+++ imageapp.py
@@ -3,7 +3,6 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import torch
 
-# Cache model and processor to reduce memory usage
 @st.cache_resource()
 def load_model_and_processor():
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
@@ -14,16 +13,13 @@ processor, model = load_model_and_processor()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# Generate caption function
 def generate_caption(image):
     inputs = processor(image, return_tensors="pt").to(device)
     out = model.generate(**inputs)
     caption = processor.decode(out[0], skip_special_tokens=True)
     return caption
 
-# Streamlit app
 st.title("Image Caption Generator")
-
 uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if st.button("Generate Caption"):
